bcfb010244e3725f0934633ac01fd20746d7514b
Dave Sarbora <dsarbora@gmail.com>
2019-02-12 14:24:46 -0800

    create method for adding item to list, and static method for displaying list

--- ToDoList.Test/ModelTests/ItemTests.cs
+++ ToDoList.Test/ModelTests/ItemTests.cs
@@ -1,18 +1,78 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System;
+using System.Collections.Generic;
 
 namespace ToDoList.Tests
 {
-  [TestClass]
-  public class ItemTest
-  {
-
-    [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    [TestClass]
+    public class ItemTest : IDisposable
     {
-      Item newItem = new Item();
-      Assert.AreEqual(typeof(Item), newItem.GetType());
+        public void Dispose()
+        {
+            Item.ClearAll();
+        }
+        [TestMethod]
+        public void Item_CanBeCreated_True()
+        {
+            Item testItem = new Item("test");
+            Assert.AreEqual (typeof(Item), testItem.GetType());
+        }
+        [TestMethod]
+        public void GetDescription_ReturnsDescription_String()
+        {
+            //Arrange
+            string description = "Walk the dog.";
+            Item testItem = new Item(description);
+            //Act
+            string result = testItem.GetDescription();
+            //Assert
+            Assert.AreEqual (description, result);
+        }
+        [TestMethod]
+        public void SetDescription_SetsDescription_String()
+        {
+            //Arrange
+            string description = "Walk the dog.";
+            Item testItem = new Item(description);
+            //Act
+            string newDescription = "Fold the laundry.";
+            testItem.SetDescription(newDescription);
+            string result = testItem.GetDescription();
+            //Assert
+            Assert.AreEqual (newDescription, result);
+        }
+        [TestMethod]
+        public void GetAll_ReturnsEmptyList_ItemList()
+        {
+            //Arrange
+            List<Item> testList = new List<Item> {};
+            //Act
+            List<Item> result = Item.GetAll();
+            foreach (Item thisItem in result)
+            {
+                Console.WriteLine("Output of empty list is " + thisItem.GetDescription());
+            }
+            //Assert
+            CollectionAssert.AreEqual (testList, result);
+        }
+        [TestMethod]
+        public void GetAll_ReturnsItems_ItemList()
+        {
+            //Arrange
+            string description1 = "Walk the dog.";
+            string description2 = "Fold the laundry";
+            Item walkDog = new Item(description1);
+            Item laundry = new Item(description2);
+            List<Item> testList = new List<Item>{walkDog, laundry};
+            //Act
+            List<Item> result = Item.GetAll();
+            foreach (Item thisItem in result)
+            {
+                Console.WriteLine("Output of filled list is " + thisItem.GetDescription());
+            }
+            //Assert
+            CollectionAssert.AreEqual (testList, result);
+        }
     }
-
-  }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,15 +1,34 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
-  public class Item
-  {
-      int X = 5;
+    public class Item
+    {
+        private string _description;
+        private static List<Item> _instances = new List<Item>{};
 
-      public Item(int x)
-      {
-          X = x;
-      }
+        public Item(string description)
+        {
+            _description = description;
+            _instances.Add(this);
+        }
 
-    // properties, methods, etc. will go here.
+        public string GetDescription()
+        {
+            return _description;
+        }
+        public void SetDescription(string input)
+        {
+            _description = input;
+        }
+        public static List<Item> GetAll()
+        {
+            return _instances;
+        }
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
 
-  }
+    }
 }
